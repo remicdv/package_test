1efd440715a7d3ef59cb6c18ccac1f5c507a6e30
colinder <colinder@metah-phi-fds-x11.imag.fr>
2024-06-11 17:42:47 +0200

    test

--- build/lib/my_package/my_module.py
+++ build/lib/my_package/my_module.py
@@ -4,10 +4,5 @@ def hello_world():
 from astropy.io import fits
 
 def print_fits_info():
-    fits_image_filename = fits.util.get_testdata_filepath('./masterflat_sdssr_18_03_2024.fits')
-
-    hdul = fits.open(fits_image_filename)
-
-    hdul.info()
-
-    hdul.close()
+    with fits.open('./my_package/masterflat_sdssr_18_03_2024.fits') as hdul:
+        hdul.info()
--- setup.py
+++ setup.py
@@ -4,5 +4,6 @@ setup(
     name='my_package',
     version='0.1',
     packages=find_packages(),
+    data_files=[("my_package",["./my_package/masterflat_sdssr_18_03_2024.fits"])],
     install_requires=['astropy'],  # Add dependencies if needed
 )
