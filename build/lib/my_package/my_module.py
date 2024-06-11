f34128e51c989b00c9be1fc11a22a7e14e1cad96
colinder <colinder@metah-phi-fds-x11.imag.fr>
2024-06-11 17:10:44 +0200

    astropy

--- build/lib/my_package/my_module.py
+++ build/lib/my_package/my_module.py
@@ -1,2 +1,12 @@
 def hello_world():
     print("Hello, world!")
+
+from astropy.io import fits
+
+fits_image_filename = fits.util.get_testdata_filepath('masterflat_sdssr_18_03_2024.fits')
+
+hdul = fits.open(fits_image_filename)
+
+hdul.info()
+
+hdul.close()
