a2feafd36b27b61138eb6bf2ca7ef97eefa500ae
colinder <colinder@metah-phi-fds-x11.imag.fr>
2024-06-12 15:32:43 +0200

    test

--- my_package/my_module.py
+++ my_package/my_module.py
@@ -6,7 +6,7 @@ from my_package import my_module
 def list_files():
     path_f = Path(my_module.__file__)
     path = Path(path_f.parent.absolute().as_posix()+'-'+metadata.version('my_package')+'.data/data/my_package')
-    for child in path.iterdir(): print(child)
+    for child in path.iterdir(): print(child.name)
 
 def hello_world():
     print("Hello, world!")
